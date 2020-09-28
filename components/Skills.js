1752eb4fb7871deaff7bb9d0f25ecc2d7d3b352a
Mikel Loidi Ardanaz <mloidi@MacBook-Pro-de-Mikel.local>
2020-09-28 17:07:51 +0200

    update print

--- components/Skills.js
+++ components/Skills.js
@@ -3,7 +3,7 @@ import React from 'react';
 export const Skills = ({ skill }) => {
   return (
     <div className='skills'>
-      <h3>{skill.type}</h3>
+      <h4>{skill.type}</h4>
       <ul>
         {skill.skills.map((skill) => (
           <li key={skill.id}>{skill.text}</li>
