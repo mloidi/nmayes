6c8cfe0168c867b2e6c594073ed9cd4cef080557
Mikel Loidi Ardanaz <mloidi@MacBook-Pro-de-Mikel.local>
2020-11-03 11:23:11 +0100

    Update blog list

--- components/PostList.js
+++ components/PostList.js
@@ -2,23 +2,55 @@ import Link from 'next/link';
 
 const PostList = ({ posts }) => {
   if (posts === 'undefined') return null;
-
+  // const tags = posts.frontmatter.tags.split(',');
   return (
-    <div>
-      {!posts && <div>No posts!</div>}
-      <ul>
-        {posts &&
-          posts.map((post) => {
-            return (
-              <li key={post.slug}>
-                <Link href={{ pathname: `/post/${post.slug}` }}>
+    <>
+      {!posts ? (
+        <div>No posts!</div>
+      ) : (
+        posts.map((post) => {
+          return (
+            <div key={post.slug} className='blogList'>
+              {post.frontmatter.coverImg && (
+                <img className='coverImg' src={post.frontmatter.coverImg} />
+              )}
+              <h2>
+                <Link href={`/post/[postname]`} as={`/post/${post.slug}`}>
                   <a>{post.frontmatter.title}</a>
                 </Link>
-              </li>
-            );
-          })}
-      </ul>
-    </div>
+              </h2>
+              <p>
+                <a
+                  className='blogAuthor'
+                  onClick={() => {
+                    alert(
+                      `Show all post publish by ${post.frontmatter.author}`
+                    );
+                  }}
+                >
+                  {post.frontmatter.author}
+                </a>
+                <span> - </span>
+                <span className='blogDate'>{post.frontmatter.date}</span>
+              </p>
+              <div className='tags'>
+                {post.frontmatter.tags.split(',').map((tag) => (
+                  <a
+                    className='tag'
+                    key={tag}
+                    onClick={() => {
+                      alert(`Show all post with ${tag.trim()} tag`);
+                    }}
+                  >
+                    #{tag.trim()}
+                  </a>
+                ))}
+              </div>
+            </div>
+          );
+        })
+      )}
+    </>
   );
 };
 
